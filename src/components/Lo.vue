af2afd332b228eaf058c18127dcef485334397e4
Milk_Tanchanok <95395296+Milktancha335@users.noreply.github.com>
2022-02-07 20:33:20 +0700

    Add files via upload

---
+++ src/components/Lo.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-card
+      class="mx-auto"
+      max-width="720"
+      v-for="(item, index) in A1"
+      :key="index"
+      height="100"
+    >
+      <v-list-item-content>
+        <center>
+          <v-list-item-subtitle>
+            {{ item.name }}
+          </v-list-item-subtitle>
+        </center>
+        <br />
+      </v-list-item-content>
+       <v-btn
+          class="mx-2 "
+          fab
+          dark
+          small
+          color="bg-danger"
+          @click="deleteTodo(index)"
+          ><v-icon dark>mdi-minus</v-icon>
+        </v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["A1"],
+  methods: {
+    deleteTodo(index) {
+      this.A1.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+//Excel
---
+++ src/components/YO.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div class="container">
+    <v-contaoner>
+      <h1>การบ้าน/งานที่ได้รับมอบหมาย</h1>
+      <v-select
+        v-model="select"
+        :items="items"
+        label="--เลือกหัวข้อ--"
+        solo
+      ></v-select>
+      <v-text-field v-model="name" label="รายการ" outlined></v-text-field>
+      <v-btn color="success" class="mr-4" @click="validate()">เพิ่ม</v-btn>
+      <v-contaoner class="">
+        <h1>รายวิชาที่เพิ่ม</h1>
+       
+      </v-contaoner>
+    </v-contaoner>
+  </div>
+</template>
+
+
+<script>
+export default {
+  subject: "",
+  work: "",
+  data() {
+    return {
+      select:null,
+      name:"",
+      items: ["วิชา Web", " วิชา SA", " วิชา ML", "อื่นๆ"],
+      selectedItem: 1,
+      additems: [
+        { text: "วิชา Web Assignment500" },
+        { text: "วิชา ML Assignment1" },
+        { text: "วิชา ML ออกแบบ" },
+        { text: "วิชา Web Assignment501" },
+      ],
+    };
+  },
+  props: ["A1"],
+  methods: {
+    validate() {
+      this.$emit("Input", this.select, this.name);
+       (this.select = ""), (this.name = "");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+//รับข้อมูล
